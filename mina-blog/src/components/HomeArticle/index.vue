a960fe923ff0f823abbe796754b1c10cac841a9b
ShimlyNa <1432979195@qq.com>
2024-08-16 18:07:25 +0800

    编写首页文章布局

--- mina-blog/src/components/HomeArticle/index.vue
+++ mina-blog/src/components/HomeArticle/index.vue
@@ -1,4 +1,34 @@
 <script setup lang="ts">
+import { watch } from "vue";
+import { useRouter } from "vue-router";
+
+const props = defineProps({
+  articleList: {
+    type: Array,
+    default: () => {},
+  },
+  articleTotal: {
+    type: Number,
+    default: 0
+  },
+  param: {
+    type: Object,
+    default: () => {},
+  },
+  category: {
+    type: Object,
+    default: () => {},
+  }
+})
+
+const router = useRouter();
+
+watch(
+    () => props.articleList,
+    () => {
+      if (!props.articleList?.length) return false;
+    }
+)
 
 </script>
 
@@ -9,3 +39,4 @@
 <style scoped>
 
 </style>
+
