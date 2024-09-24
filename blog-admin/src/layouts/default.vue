0d4059d6f6f030030b0907321b4cce6a14f268bc
ShimlyNa <1432979195@qq.com>
2024-09-24 17:27:14 +0800

    设置根路径下的页面，非home页面，未完

--- blog-admin/src/layouts/default.vue
+++ blog-admin/src/layouts/default.vue
@@ -1,11 +1,11 @@
 <template>
-  <v-layout class="rounded rounded-md">
+  <v-layout class="rounded rounded-md mi-layout">
     <Header />
     <Side />
     <Main />
-<!--    <v-main class="d-flex align-center justify-center" style="min-height: 300px;">-->
-<!--      Main Content-->
-<!--    </v-main>-->
+    <!--    <v-main class="d-flex align-center justify-center" style="min-height: 300px;">-->
+    <!--      Main Content-->
+    <!--    </v-main>-->
   </v-layout>
 </template>
 
@@ -15,3 +15,6 @@
   import Side from './default/sider/index.vue'
   import Main from './default/main/index.vue'
 </script>
+<style lang="scss" scoped>
+
+</style>
--- blog-admin/src/pages/index.vue
+++ blog-admin/src/pages/index.vue
@@ -1,21 +1,25 @@
 <template>
   <v-row class="pageIndex">
     <v-col>
-      <v-container class="mi-home">
-        <h2>欢迎回来！<span>米娜</span></h2>
-        <v-divider />
-        <v-row>
-          <v-col>111</v-col>
-          <v-col>111</v-col>
-          <v-col>111</v-col>
-          <v-col>111</v-col>
-        </v-row>
-      </v-container>
+      <v-card>
+        <v-container class="mi-home">
+          <h2>欢迎回来！<span>米娜</span></h2>
+          <v-divider />
+          <v-row>
+            <v-col>111</v-col>
+            <v-col>111</v-col>
+            <v-col>111</v-col>
+            <v-col>111</v-col>
+          </v-row>
+        </v-container>
+      </v-card>
     </v-col>
     <v-col>
-      <v-container>
-        222
-      </v-container>
+      <v-card>
+        <v-container>
+          222
+        </v-container>
+      </v-card>
     </v-col>
   </v-row>
 </template>
@@ -26,7 +30,7 @@
 
 <style scoped lang="scss">
 .pageIndex {
-  background-color: #fff;
+  //background-color: #fff;
   margin: 10px;
 }
 
